00156d4007a09715f96b48c40971727ad80c052f
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2020-07-05 22:42:30 -0300

    Changed: Changing to final instead

--- src/TemperatureTestClass.php
+++ src/TemperatureTestClass.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace RigorTalks;
 
-use Doctrine\DBAL\DriverManager;
-use Doctrine\DBAL\Configuration;
-use RigorTalks\Exceptions\TemperatureNegativeException;
-
-class TemperatureTestClass extends Temperature
+final class TemperatureTestClass extends Temperature
 {
     /**
      * Implementation class to test coupled code
